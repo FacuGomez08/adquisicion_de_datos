9becce968fd6c3f49b917750fedb04a0acef31e9
alumno <santiblanco7126@gmail.com>
2024-04-19 11:25:53 -0300

    agrego main.c

--- actividades/actividad_1/main.c
+++ actividades/actividad_1/main.c
@@ -3,52 +3,38 @@
 #include <math.h>
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
-#include "hardware/i2c.h"
-#include "lcd_i2c.h"
-
 /*
  * @brief Programa principal
  */ 
 int main() {
     // Variable para almacenar el resultado del ADC
-    uint16_t adc_value = 0;
+    uint16_t NTC = 0;
     // Variable para guardar el valor de temperatura
     float temperatura = 0.0;
     // Constante de proporcionalidad del termistor
-    const uint16_t beta = 3950;
+    const uint16_t beta = 4000;
     // Habilito USB
     stdio_init_all();
-    // Configuro el I2C0 a 100 KHz de clock
-    i2c_init(i2c0, 100 * 1000);
-    // Elijo GPIO4 como linea de SDA
-    gpio_set_function(4, GPIO_FUNC_I2C);
-    // Elijo GPIO5 como linea de SCL
-    gpio_set_function(5, GPIO_FUNC_I2C);
-    // Activo pull-up en ambos GPIO, son debiles por lo que
-    // es recomendable usar pull-ups externas
-    gpio_pull_up(4);
-    gpio_pull_up(5);
-    // Inicializacion del LCD
-    lcd_init();
     // Inicializo ADC
-
+    adc_init();
     // Inicializo GPIO26 como entrada analogica
-
+    adc_gpio_init(26);
     // Selecciono canal analogico
-
+    adc_select_input(0);
     while(true) {
-        // Leer NTC
+
+        temperatura = 0;
+        for (int i=0; i<10; i++){
+             // Leer NTC
+            NTC = adc_read();
+            // Calculo temperatura
+            float vx = (NTC * 3.3)/4095;
+            float rt =  3300/((3.3/vx)-1);
+            temperatura = ((beta*298.15)/( (298*log(rt/2200))+beta))-273;
+        }
         
-        // Calculo temperatura
-        
-        // Limpio LCD
-        lcd_clear();
-        // Variable para el string
-        char str[16];
-        // Creo string
-        
-        // Imprimo string en segunda fila
-        lcd_string(str);
+        temperatura = temperatura/10;
+        printf("La temperatura es %.2f\n", temperatura);
         // Espero 500 ms
         sleep_ms(500);
     }
